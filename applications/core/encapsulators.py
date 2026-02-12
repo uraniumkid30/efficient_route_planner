ca3703a822c706ec56b9e6a956fa29ffa6b6d64f
christopher okoro <uraniumkid30@gmail.com>
2026-02-12 09:38:09 +0100

    Added private and public implementation, python lacks this

---
+++ applications/core/encapsulators.py
@@ -0,0 +1,32 @@
+def __accessDecorator(failIf):
+    def onDecorator(aClass):
+        class onInstance:
+            def __init__(self, *args, **kargs):
+                self.__wrapped = aClass(*args, **kargs)
+
+            def __getattr__(self, attr):
+                if failIf(attr):
+                    raise TypeError("private attribute fetch: " + attr)
+                else:
+                    return getattr(self.__wrapped, attr)
+
+            def __setattr__(self, attr, value):
+
+                if attr == "_onInstance__wrapped":
+                    self.__dict__[attr] = value
+                elif failIf(attr):
+                    raise TypeError("private attribute change: " + attr)
+                else:
+                    setattr(self.__wrapped, attr, value)
+
+        return onInstance
+
+    return onDecorator
+
+
+def Private(*attributes):
+    return __accessDecorator(failIf=(lambda attr: attr in attributes))
+
+
+def Public(*attributes):
+    return __accessDecorator(failIf=(lambda attr: attr not in attributes))
